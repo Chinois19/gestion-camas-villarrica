be30df63e2ea870cbf4bc70ab267471e186c40f3
Hospital Villarrica Control Gestión y Produc19 <vllhcontrol19@minsal.cl>
2026-08-27 10:52:12 -0400

    Archivos .env de separación de credenciales de base de datos desarrollo-producción

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBIdM0cYhzO03k4nGjJH3W906R2xeRBpso",
-  authDomain: "gestion-camas-villarrica.firebaseapp.com",
-  projectId: "gestion-camas-villarrica",
-  storageBucket: "gestion-camas-villarrica.firebasestorage.app",
-  messagingSenderId: "224302432807",
-  appId: "1:224302432807:web:ef62069f0b1e4b64298402"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
